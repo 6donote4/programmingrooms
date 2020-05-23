02afc31d4c327b8eda3633b70de5fe66b20b1829
6donote4 <6donote4@gmail.com>
2020-05-23 19:14:45 +0800

    new trainning 20200523

---
+++ python/2/func_return.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+
+def maximum(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+
+print maximum(2,3)
---
+++ python/2/mymodule.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2
+
+def sayhi():
+    print 'Hi,this is mymodule speaking.'
+
+version = '0.1'
+
